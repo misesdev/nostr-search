9e5be264d228530411a089f8c24f4a5ab7f7934b
Mises Dev <misesdev@onionmail.org>
2025-02-05 10:19:20 -0300

    string similarity refactory for fix colisions

--- engine/src/utils/string_utils.c
+++ engine/src/utils/string_utils.c
@@ -1,9 +1,10 @@
 #ifndef STRING_UTILS_C
 #define STRING_UTILS_C
 
-#include <ctype.h>
 #include <stdint.h>
+#include "./char_map.c"
 
+#define HASH_PRIME 37
 #define HASH_SIZE 10000
 
 float textSimilarity(char *origin, char *compare) 
@@ -12,23 +13,27 @@ float textSimilarity(char *origin, char *compare)
     uint_fast32_t intersection = 0, number = 0, sizePairs = 0; 
     uint_fast64_t hashSet[HASH_SIZE / 64] = {0};
 
-    while(*(origin + 1)) 
+    unsigned char *orig = (unsigned char*) origin;
+    while(*(orig + 1)) 
     { 
-        number = (uint32_t) ((tolower(*origin) * 100) + tolower(*(origin+1))) % HASH_SIZE;
-        if((hashSet[number / 64] & 1UL << (number % 64)) == 0)
-            hashSet[number / 64] |= 1UL << (number % 64);
-
+        unsigned char a = char_map[*orig] ? char_map[*orig]: *orig;
+        unsigned char b = char_map[*(orig+1)] ? char_map[*(orig+1)]: *(orig+1);
+        number = (a * HASH_PRIME + b) % HASH_SIZE; //multiplication with prime number
+        hashSet[number / 64] |= 1UL << (number % 64);
         sizePairs++;
-        origin++;
+        orig++;
     }
 
-    while(*(compare + 1)) 
+    unsigned char *comp = (unsigned char*) compare;
+    while(*(comp + 1)) 
     { 
-        number = (uint32_t) ((tolower(*compare) * 100) + tolower(*(compare+1))) % HASH_SIZE;        
-        if(hashSet[number / 64] & 1UL << (number % 64)) intersection++;
-
+        unsigned char a = char_map[*comp] ? char_map[*comp]: *comp;
+        unsigned char b = char_map[*(comp+1)] ? char_map[*(comp+1)]: *(comp+1);
+        number = (a * HASH_PRIME + b) % HASH_SIZE; //multiplication with prime number
+        if(hashSet[number / 64] & 1UL << (number % 64)) 
+            intersection++;
         sizePairs++;
-        compare++;
+        comp++;
     }
 
     return sizePairs > 0 ? (2.0 * intersection) / sizePairs : 0.0;
